b23607613ef7949a5a7a71d1b3c028e5f4b1fb50
Martin SIREAU <msireau@student.42.fr>
2017-10-14 09:44:15 +0200

    table view ok + table view cell custom

---
+++ d09/d09/ArticleTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  ArticleTableViewCell.swift
+//  d09
+//
+//  Created by Martin SIREAU on 10/14/17.
+//  Copyright © 2017 Martin SIREAU. All rights reserved.
+//
+
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var myTitle: UILabel!
+    @IBOutlet weak var imageLabel: UIImageView!
+    
+    @IBOutlet weak var contentLabel: UILabel!
+    @IBOutlet weak var creationLabel: UILabel!
+    @IBOutlet weak var modificationLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- d09/d09/ScndViewController.swift
+++ d09/d09/ScndViewController.swift
@@ -7,16 +7,21 @@
 //
 
 import UIKit
+import msireau2017
 
 class ScndViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
 
     var arr = ["lol", "lol", "lol"]
     @IBOutlet weak var myTableVC: UITableView!
     
+//    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.title = "My Articles"
+//        let articleManager = ArticleManager(managedObjectContext: self.context)
+
         //access to core data
     }
     
@@ -25,9 +30,12 @@ class ScndViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell:UITableViewCell=UITableViewCell(style: UITableViewCellStyle.subtitle, reuseIdentifier: "myCell")
+        let cell : ArticleTableViewCell = self.myTableVC.dequeueReusableCell(withIdentifier: "myCell") as! ArticleTableViewCell
         
-        cell.textLabel?.text = arr[indexPath.row]
+        cell.myTitle.text = arr[indexPath.row]
+        cell.contentLabel.text = arr[indexPath.row]
+        cell.creationLabel.text = arr[indexPath.row]
+        cell.modificationLabel.text = arr[indexPath.row]
         return cell
     }
 }
